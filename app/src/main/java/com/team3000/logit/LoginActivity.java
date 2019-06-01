a0858f5b7d14e00205b1ff898749f425c6b4dcf8
Chris Kheng <chriskheng1609@gmail.com>
2019-06-01 11:25:11 +0800

    Create sign up page and link it with login page

---
+++ app/src/main/java/com/team3000/logit/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.team3000.logit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.SignInButton;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView signUpLink;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        signUpLink = findViewById(R.id.signUpLink);
+        setClickListeners();
+    }
+
+    private void setClickListeners() {
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
