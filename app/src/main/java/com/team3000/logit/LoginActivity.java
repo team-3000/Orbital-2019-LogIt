df749b80b73023e038787da68d725bfbc7f1e18b
Chris Kheng <chriskheng1609@gmail.com>
2019-06-01 14:42:28 +0800

    Introduce login functionality

--- app/src/main/java/com/team3000/logit/LoginActivity.java
+++ app/src/main/java/com/team3000/logit/LoginActivity.java
@@ -1,17 +1,30 @@
 package com.team3000.logit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private FirebaseAuth mAuth;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button loginButton;
     private TextView signUpLink;
 
     @Override
@@ -19,11 +32,25 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        // Find all the necessary fields
+        mEmailField = findViewById(R.id.emailField);
+        mPasswordField = findViewById(R.id.passwordField);
+        loginButton = findViewById(R.id.login_button);
         signUpLink = findViewById(R.id.signUpLink);
+
         setClickListeners();
     }
 
     private void setClickListeners() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn();
+            }
+        });
+
         signUpLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,5 +58,65 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void logIn() {
+        String email = mEmailField.getText().toString().trim();
+        String password = mPasswordField.getText().toString();
+
+        if (validateForm(email, password)) {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                // Only update the UI if user has verified their account
+                                // through email
+                                if (user.isEmailVerified()) {
+                                    updateUI(user);
+                                } else {
+                                    mAuth.signOut();
+                                    Toast.makeText(LoginActivity.this, "" +
+                                                    "Either email or password entered is incorrect" +
+                                                    "or have not performed email verification",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Validate the signUp form
+     * @return a boolean indicating whether the signUp form is valid
+     */
+    private boolean validateForm(String email, String password) {
+        boolean isValid = true;
+
+        // Check all the fields
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Required.");
+            isValid = false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Required.");
+            isValid = false;
+        }
+
+        return isValid;
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Toast.makeText(this, "Username is " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
     }
 }
