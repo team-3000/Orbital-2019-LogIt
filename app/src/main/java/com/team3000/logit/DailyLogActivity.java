f02ace7d049112267ad5c6ce9845e6bf88df3be4
Chris Kheng <chriskheng1609@gmail.com>
2019-06-11 16:19:11 +0800

    Fix navigation drawer performance issue

--- app/src/main/java/com/team3000/logit/DailyLogActivity.java
+++ app/src/main/java/com/team3000/logit/DailyLogActivity.java
@@ -2,18 +2,15 @@ package com.team3000.logit;
 
 import android.os.Bundle;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 public class DailyLogActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Load the content of the frame layout accordingly.
+        // The frame layout serves as a container for the content you want to put.
         FrameLayout contentFrameLayout = findViewById(R.id.content_frame);
-        if (contentFrameLayout != null) {
-            Toast.makeText(this, "Not null", Toast.LENGTH_SHORT).show();
-        }
-
         getLayoutInflater().inflate(R.layout.activity_daily_log, contentFrameLayout);
     }
 }
