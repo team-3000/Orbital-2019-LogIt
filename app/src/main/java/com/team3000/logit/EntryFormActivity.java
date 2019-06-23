6826a6d145a0de36251ce17c2d194973a41f59f7
lye-jw <lyejianwen@gmail.com>
2019-06-24 00:33:19 +0800

    Code quality fixes

--- app/src/main/java/com/team3000/logit/EntryFormActivity.java
+++ app/src/main/java/com/team3000/logit/EntryFormActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-// import android.widget.CheckBox;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
