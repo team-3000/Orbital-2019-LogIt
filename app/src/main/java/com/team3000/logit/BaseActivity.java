5e2a7245d9f93356db918f93f69f7645fce7669d
Chris Kheng <chriskheng1609@gmail.com>
2019-07-09 21:57:24 +0800

    Fix code style issue

--- app/src/main/java/com/team3000/logit/BaseActivity.java
+++ app/src/main/java/com/team3000/logit/BaseActivity.java
@@ -162,32 +162,23 @@ public abstract class BaseActivity extends AppCompatActivity
             return true;
         }
 
-        Intent intent;
-        switch(id) {
-            case R.id.nav_today :
-                intent = new Intent(BaseActivity.this, DailyLogActivity.class)
-                        .putExtra("year", 0)
-                        .putExtra("isTodayDailyLog", true);
-                break;
-            case R.id.nav_this_month :
-                intent = new Intent(BaseActivity.this, MonthlyLogActivity.class)
-                        .putExtra("year", 0);
-                break;
-            case R.id.nav_calendar :
-                intent = new Intent(BaseActivity.this, CalendarActivity.class);
-                break;
-            case R.id.nav_collections :
-                intent = new Intent(BaseActivity.this, CollectionListActivity.class);
-                break;
-            case R.id.nav_eisen :
-                intent = new Intent(BaseActivity.this, EisenhowerActivity.class);
-                break;
-            case R.id.nav_signOut :
-                mAuth.signOut();
-                intent = new Intent(this, LoginActivity.class);
-                break;
-            default:
-                intent = null;
+        Intent intent = null;
+        if (id == R.id.nav_today) {
+            intent = new Intent(BaseActivity.this, DailyLogActivity.class)
+                    .putExtra("year", 0)
+                    .putExtra("isTodayDailyLog", true);
+        } else if (id == R.id.nav_this_month) {
+            intent = new Intent(BaseActivity.this, MonthlyLogActivity.class)
+                    .putExtra("year", 0);
+        } else if (id == R.id.nav_calendar) {
+            intent = new Intent(BaseActivity.this, CalendarActivity.class);
+        } else if (id == R.id.nav_collections) {
+            intent = new Intent(BaseActivity.this, CollectionListActivity.class);
+        } else if (id == R.id.nav_eisen) {
+            intent = new Intent(BaseActivity.this, EisenhowerActivity.class);
+        } else if (id == R.id.nav_signOut) {
+            mAuth.signOut();
+            intent = new Intent(this, LoginActivity.class);
         }
 
         // new stuff (remember to reflect any newly added extra in the intent in onBackPressed method
