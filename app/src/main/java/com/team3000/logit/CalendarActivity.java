cff60f8d58b7158c3ccb632e7cf6224d5aa32508
lye-jw <lyejianwen@gmail.com>
2019-07-14 17:05:25 +0800

    UI & code quality fixes

--- app/src/main/java/com/team3000/logit/CalendarActivity.java
+++ app/src/main/java/com/team3000/logit/CalendarActivity.java
@@ -2,7 +2,6 @@ package com.team3000.logit;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
