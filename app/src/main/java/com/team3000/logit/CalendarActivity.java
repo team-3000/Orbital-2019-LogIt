bfc2b84390c5488f5e3f5e083171e951d58fb884
lye-jw <lyejianwen@gmail.com>
2019-07-15 16:51:49 +0800

    Code quality fixes

--- app/src/main/java/com/team3000/logit/CalendarActivity.java
+++ app/src/main/java/com/team3000/logit/CalendarActivity.java
@@ -2,7 +2,6 @@ package com.team3000.logit;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -12,7 +11,6 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.text.DateFormatSymbols;
-import java.util.Calendar;
 
 public class CalendarActivity extends BaseActivity {
 
