64f327beb69cec89e0ecd886d53464bc048b7659
lye-jw <lyejianwen@gmail.com>
2019-07-17 22:38:40 +0800

    Implement Delete confirmation dialog

--- app/src/main/java/com/team3000/logit/EntryActivity.java
+++ app/src/main/java/com/team3000/logit/EntryActivity.java
@@ -1,5 +1,7 @@
 package com.team3000.logit;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
 import android.view.View;
@@ -127,16 +130,26 @@ public class EntryActivity extends BaseActivity {
         });
 
         btnDeleteEntry.setOnClickListener(v -> {
-            EntryManager entryManager = new EntryManager(EntryActivity.this);
-            entryManager.deleteFromTracker(type + "Store", directory);
-            if (!"".equals(eisen)) {
-                entryManager.deleteFromTracker(eisen, directory);
-            }
-            // ref.delete();
-//            startActivity(new Intent(EntryActivity.this, DailyLogActivity.class));
-            // The EntryActivity will straightaway close once item is deleted in Firestore (handled in deleteEntry())
-            entryManager.deleteEntry(ref);
-//            EntryActivity.this.finish();
+            AlertDialog.Builder builder = new AlertDialog.Builder(EntryActivity.this);
+            builder.setMessage("Delete \"" + tvEntryTitle.getText().toString() + "\"?")
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            EntryManager entryManager = new EntryManager(EntryActivity.this);
+                            entryManager.deleteFromTracker(type + "Store", directory);
+                            if (!"".equals(eisen)) {
+                                entryManager.deleteFromTracker(eisen, directory);
+                            }
+                            // The EntryActivity will straightaway close once item is deleted in Firestore (handled in deleteEntry())
+                            entryManager.deleteEntry(ref);
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
         });
     }
 
