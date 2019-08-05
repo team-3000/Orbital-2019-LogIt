72370740b77db3590db48efc7265f92b2dd777e7
Chris Kheng <chriskheng1609@gmail.com>
2019-08-05 18:14:32 +0800

    Squash bug when navigating from other daily's log to today daily log

--- app/src/main/java/com/team3000/logit/DailyLogActivity.java
+++ app/src/main/java/com/team3000/logit/DailyLogActivity.java
@@ -1,6 +1,7 @@
 package com.team3000.logit;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,7 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 public class DailyLogActivity extends BaseLogActivity {
@@ -19,13 +21,16 @@ public class DailyLogActivity extends BaseLogActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        /*
         // For today's daily log
         if (Calendar.getInstance().get(Calendar.DAY_OF_MONTH) == day) {
             super.currPosition = R.id.nav_today;
         }
+        */
 
         logDate = String.format(Locale.US, "%d %s %d", day, month, year);
         getSupportActionBar().setTitle(logDate);
+
 
         mPager = findViewById(R.id.log_pager);
         pagerAdapter = new BaseLogPagerAdapter(getSupportFragmentManager());
@@ -33,6 +38,27 @@ public class DailyLogActivity extends BaseLogActivity {
 
         TabLayout tabs = findViewById(R.id.logTabLayout);
         tabs.setupWithViewPager(mPager);
+    }
+
+    // The logic specified inside this method is to deal with side navigation drawer issue
+    // It is to enable the app to navigate to today's daily log through the navigation drawer
+    // when the user is currently at a daily log that is not today's.
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // MUST Reset the currPosition
+        super.currPosition = R.id.daily_log_activity;
+
+        Calendar calendar = Calendar.getInstance();
+        String currMonth = calendar.getDisplayName(Calendar.MONTH, Calendar.SHORT,
+                Locale.US);
+        int currDay = calendar.get(Calendar.DAY_OF_MONTH);
+        int currYear = calendar.get(Calendar.YEAR);
+
+        if (currDay == day && currMonth.equals(month) && currYear == year) {
+            super.currPosition = R.id.nav_today;
+        }
     }
 
     @Override
